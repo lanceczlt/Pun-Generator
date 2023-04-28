28710d1f4eebd5c575dbc4633c1f9626c44c185d
wan-cs <jdw159@case.edu>
2023-04-28 14:53:09 -0400

    update query route to handle filters

--- flask-server/server.py
+++ flask-server/server.py
@@ -16,27 +16,27 @@ def home():
     return "<h1>Hello World<h1>"
 
 
-@app.route('/query', methods=['GET', 'POST'])
+@app.route('/query', methods=['POST'])
 def query():
-    # if request.method == 'GET':
-    #     return "<h1>success<h1>"
     if request.method == 'POST':
         data = json.loads(request.data)
         word = data["input"]
+        filters = data["filters"]
 
-        # try:
-        output = subprocess.check_output(
-            ['python', '../src/query/query_rhymes.py', '../src/my_db.sqlite', word, '--mode', 'word'])
+        filter_list = []
+        for filter_name, isSelected in filters.items():
+            if isSelected:
+                filter_list.append(filter_name)
+
+        filter_str = json.dumps(filter_list)
+
+        try:
+            output = subprocess.check_output(
+                ['python', '../src/query/query_rhymes.py', '../src/my_db.sqlite', word, '--filters', filter_str, '--mode', 'word'])
+        except subprocess.CalledProcessError as e:
+            output = e.output
+
         return jsonify({'output': output.decode('utf-8')})
-        # except subprocess.CalledProcessError as e:
-        #     print(e.returncode)
-        #     pass
-
-        # @app.route('/test')
-        # def run_script():
-        #     output = subprocess.check_output(
-        #         ['python', './src/query/query_rhymes.py', './db.sqlite', 'rye', '--mode', 'word'])
-        #     return jsonify({'output': output.decode('utf-8')})
 
 
 if __name__ == "__main__":
